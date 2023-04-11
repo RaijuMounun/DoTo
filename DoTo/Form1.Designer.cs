83e1c627086901207b1ee585c1891168a2a80148
Eren Keskinoğlu <erenkeskinoglu@outlook.com>
2023-04-11 14:22:35 +0300

    Project Upload

---
+++ DoTo/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace DoTo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.todoPanel = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.controlPanel = new System.Windows.Forms.Panel();
+            this.notuDuzenleBtn = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.NotuSilBtn = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btnYeniNotEkle = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.dtp = new System.Windows.Forms.DateTimePicker();
+            this.NotuGoruntuleBtn = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.todoPanel.SuspendLayout();
+            this.controlPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // todoPanel
+            // 
+            this.todoPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.todoPanel.BackColor = System.Drawing.Color.Red;
+            this.todoPanel.Controls.Add(this.listView1);
+            this.todoPanel.Controls.Add(this.controlPanel);
+            this.todoPanel.Location = new System.Drawing.Point(12, 91);
+            this.todoPanel.Name = "todoPanel";
+            this.todoPanel.Size = new System.Drawing.Size(855, 461);
+            this.todoPanel.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10,
+            this.columnHeader11});
+            this.listView1.Font = new System.Drawing.Font("LEMON MILK Light", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(544, 461);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate_1);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 600;
+            // 
+            // controlPanel
+            // 
+            this.controlPanel.BackColor = System.Drawing.Color.Yellow;
+            this.controlPanel.Controls.Add(this.NotuGoruntuleBtn);
+            this.controlPanel.Controls.Add(this.notuDuzenleBtn);
+            this.controlPanel.Controls.Add(this.NotuSilBtn);
+            this.controlPanel.Controls.Add(this.btnYeniNotEkle);
+            this.controlPanel.Controls.Add(this.dtp);
+            this.controlPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.controlPanel.Location = new System.Drawing.Point(550, 0);
+            this.controlPanel.Name = "controlPanel";
+            this.controlPanel.Size = new System.Drawing.Size(305, 461);
+            this.controlPanel.TabIndex = 0;
+            // 
+            // notuDuzenleBtn
+            // 
+            this.notuDuzenleBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.notuDuzenleBtn.Depth = 0;
+            this.notuDuzenleBtn.Location = new System.Drawing.Point(38, 304);
+            this.notuDuzenleBtn.MouseState = MaterialSkin.MouseState.HOVER;
+            this.notuDuzenleBtn.Name = "notuDuzenleBtn";
+            this.notuDuzenleBtn.Primary = true;
+            this.notuDuzenleBtn.Size = new System.Drawing.Size(140, 23);
+            this.notuDuzenleBtn.TabIndex = 3;
+            this.notuDuzenleBtn.Text = "Notu Düzenle";
+            this.notuDuzenleBtn.UseVisualStyleBackColor = true;
+            this.notuDuzenleBtn.Click += new System.EventHandler(this.notuDuzenleBtn_Click);
+            // 
+            // NotuSilBtn
+            // 
+            this.NotuSilBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NotuSilBtn.Depth = 0;
+            this.NotuSilBtn.Location = new System.Drawing.Point(38, 333);
+            this.NotuSilBtn.MouseState = MaterialSkin.MouseState.HOVER;
+            this.NotuSilBtn.Name = "NotuSilBtn";
+            this.NotuSilBtn.Primary = true;
+            this.NotuSilBtn.Size = new System.Drawing.Size(140, 23);
+            this.NotuSilBtn.TabIndex = 2;
+            this.NotuSilBtn.Text = "Notu Sil";
+            this.NotuSilBtn.UseVisualStyleBackColor = true;
+            this.NotuSilBtn.Click += new System.EventHandler(this.NotuSilBtn_Click);
+            // 
+            // btnYeniNotEkle
+            // 
+            this.btnYeniNotEkle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnYeniNotEkle.Depth = 0;
+            this.btnYeniNotEkle.Location = new System.Drawing.Point(38, 246);
+            this.btnYeniNotEkle.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnYeniNotEkle.Name = "btnYeniNotEkle";
+            this.btnYeniNotEkle.Primary = true;
+            this.btnYeniNotEkle.Size = new System.Drawing.Size(140, 23);
+            this.btnYeniNotEkle.TabIndex = 1;
+            this.btnYeniNotEkle.Text = "Yeni Not";
+            this.btnYeniNotEkle.UseVisualStyleBackColor = true;
+            this.btnYeniNotEkle.Click += new System.EventHandler(this.btnYeniNotEkle_Click);
+            // 
+            // dtp
+            // 
+            this.dtp.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dtp.Location = new System.Drawing.Point(17, 19);
+            this.dtp.Name = "dtp";
+            this.dtp.Size = new System.Drawing.Size(276, 22);
+            this.dtp.TabIndex = 0;
+            // 
+            // NotuGoruntuleBtn
+            // 
+            this.NotuGoruntuleBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NotuGoruntuleBtn.Depth = 0;
+            this.NotuGoruntuleBtn.Location = new System.Drawing.Point(38, 275);
+            this.NotuGoruntuleBtn.MouseState = MaterialSkin.MouseState.HOVER;
+            this.NotuGoruntuleBtn.Name = "NotuGoruntuleBtn";
+            this.NotuGoruntuleBtn.Primary = true;
+            this.NotuGoruntuleBtn.Size = new System.Drawing.Size(140, 23);
+            this.NotuGoruntuleBtn.TabIndex = 4;
+            this.NotuGoruntuleBtn.Text = "Notu Görüntüle";
+            this.NotuGoruntuleBtn.UseVisualStyleBackColor = true;
+            this.NotuGoruntuleBtn.Click += new System.EventHandler(this.NotuGoruntuleBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(879, 564);
+            this.Controls.Add(this.todoPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.todoPanel.ResumeLayout(false);
+            this.controlPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel todoPanel;
+        private System.Windows.Forms.Panel controlPanel;
+        private System.Windows.Forms.DateTimePicker dtp;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+        private MaterialSkin.Controls.MaterialRaisedButton btnYeniNotEkle;
+        private MaterialSkin.Controls.MaterialRaisedButton NotuSilBtn;
+        private MaterialSkin.Controls.MaterialRaisedButton notuDuzenleBtn;
+        private MaterialSkin.Controls.MaterialRaisedButton NotuGoruntuleBtn;
+    }
+}
+
---
+++ DoTo/Form1.cs
@@ -0,0 +1,339 @@
+﻿#region Using
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+#endregion
+
+namespace DoTo
+{
+    public partial class Form1 : MaterialForm
+    {
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+
+            TemaRengi(Primary.DeepPurple800, Primary.DeepPurple800, Primary.DeepPurple800, Accent.Indigo700, TextShade.WHITE);
+            AppbarDuzenle();
+        }
+        #endregion
+
+        //Helper Fonskiyonlar
+        #region TemaRengi
+        private void TemaRengi(Primary renk1, Primary renk2, Primary renk3, Accent accent, TextShade textShade)
+        {
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(renk1, renk2, renk3, accent, textShade);
+            controlPanel.BackColor = Color.BlueViolet;
+            listView1.BackColor = Color.BlueViolet;
+        }
+        #endregion
+
+        #region AppBar Duzenle
+        private void AppbarDuzenle()
+        {
+            this.Text = "DoTo";
+            this.MinimumSize = new Size(760, 390);
+        }
+        #endregion
+
+        #region NotlariOkuListele
+        private void NotlariOkuListele()
+        {            
+            string dosyaYolu = "notlar.txt";
+
+            // Dosyadaki notları oku ve tamnot dizisine ekle
+            string dosya;
+            string[] tamNot;
+            using (StreamReader sr = new StreamReader(dosyaYolu))
+            {                
+                dosya = sr.ReadToEnd();
+                tamNot = dosya.Split('$');                
+            }
+
+            // tamNot dizisinin ilk elemanı boş geliyordu, bu kısım onu düzeltmek için
+            var gecicitamNot = tamNot.ToList();
+            gecicitamNot.RemoveAt(0);
+            tamNot = gecicitamNot.ToArray(); 
+
+            // Notları listview'a ekle
+            foreach (string not in tamNot)
+            {
+                string[] notBilgileri = not.Split(';');
+
+                
+                ListViewItem item = new ListViewItem();
+                item.Text = notBilgileri[1] + " | " + notBilgileri[0];
+                item.SubItems.Add(notBilgileri[1]);
+                item.SubItems.Add(notBilgileri[2]);
+                item.SubItems.Add(notBilgileri[0]);
+                listView1.Items.Add(item);
+            }
+        }
+        #endregion
+
+        #region Notu Sil
+        private void NotuSil()
+        {
+            string dosyaYolu = "notlar.txt";
+            string dosya = "";
+
+            // Notları oku
+            using (StreamReader sr = new StreamReader(dosyaYolu))
+            {
+                dosya = sr.ReadToEnd();
+            }
+
+            // Seçilen item'ın tüm subitemlarını birleştirerek notu elde et
+            string secilenNot = "$" + listView1.SelectedItems[0].SubItems[3].Text + ";"
+                                    + listView1.SelectedItems[0].SubItems[1].Text + ";"
+                                    + listView1.SelectedItems[0].SubItems[2].Text;
+
+            // Dosyadan seçilenNot ile eşleşen notu sil
+            dosya = dosya.Replace(secilenNot, "");
+
+            // Dosyaya yeni halini yaz
+            using (StreamWriter sw = new StreamWriter(dosyaYolu, false))
+            {
+                sw.Write(dosya);
+            }
+
+            // ListView'dan seçilen item'ı sil
+            listView1.SelectedItems[0].Remove();
+        }
+        #endregion
+
+        #region Yeni Not Ekle
+        private void YeniNotEkle()
+        {
+            // Yeni bir form aç
+            Form yeniNotForm = new Form();
+            yeniNotForm.Text = "Yeni Not Ekle";
+
+            // Not başlığı için metin kutusu oluştur
+            Label lblBaslik = new Label();
+            lblBaslik.Text = "Başlık:";
+            lblBaslik.Location = new Point(10, 10);
+            yeniNotForm.Controls.Add(lblBaslik);
+
+            TextBox txtBaslik = new TextBox();
+            txtBaslik.Location = new Point(10, 30);
+            yeniNotForm.Controls.Add(txtBaslik);
+
+            // Not detayları için metin alanı oluştur
+            Label lblDetaylar = new Label();
+            lblDetaylar.Text = "Detaylar:";
+            lblDetaylar.Location = new Point(10, 60);
+            yeniNotForm.Controls.Add(lblDetaylar);
+
+            TextBox txtDetaylar = new TextBox();
+            txtDetaylar.Multiline = true;
+            txtDetaylar.Size = new Size(200, 100);
+            txtDetaylar.Location = new Point(10, 80);
+            yeniNotForm.Controls.Add(txtDetaylar);
+
+            // Kaydet butonu oluştur
+            Button btnKaydet = new Button();
+            btnKaydet.Text = "Kaydet";
+            btnKaydet.Location = new Point(10, 190);
+            yeniNotForm.Controls.Add(btnKaydet);
+
+            // Kaydet butonuyla notu listview'e ekle
+            btnKaydet.Click += (sender2, e2) =>
+            {
+                DateTime txtTarih = dtp.Value;
+                ListViewItem item = new ListViewItem();
+                item.Text = txtBaslik.Text + " | " + dtp.Value.ToShortDateString();
+                item.SubItems.Add(txtBaslik.Text);
+                item.SubItems.Add(txtDetaylar.Text);
+                item.SubItems.Add(txtTarih.ToShortDateString());
+                listView1.Items.Add(item);
+
+                // Notları dosyaya kaydet
+                string dosyaYolu = "notlar.txt";
+                using (StreamWriter sw = new StreamWriter(dosyaYolu, true))
+                {
+                    sw.WriteLine("$" + txtTarih.ToShortDateString() + ";" + txtBaslik.Text + ";" + txtDetaylar.Text);
+                }
+
+                yeniNotForm.Close(); // Formu kapat
+            };
+
+            yeniNotForm.ShowDialog(); // Formu göster
+        }
+        #endregion
+
+        //------------------------------------------------------------------------------------------
+
+        #region Form Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtp.Left = (controlPanel.Width - dtp.Width) / 2;
+            NotlariOkuListele();
+            NotuSilBtn.Visible = NotuGoruntuleBtn.Visible = notuDuzenleBtn.Visible = false;
+            listView1.Font = new Font("LEMON MILK Light", 12);
+            NotuSilBtn.Parent = btnYeniNotEkle.Parent = notuDuzenleBtn.Parent = NotuGoruntuleBtn.Parent = controlPanel;
+            this.ForeColor = Color.Indigo;
+        }
+        #endregion
+
+        #region Form Resize
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            controlPanel.Width = todoPanel.Width / 3;
+            dtp.Left = (controlPanel.Width - dtp.Width) / 2;
+            listView1.Width = todoPanel.Width - controlPanel.Width;
+        }
+        #endregion
+
+        #region Listview item activate
+        private void listView1_ItemActivate_1(object sender, EventArgs e)
+        {
+            // Seçilen öğenin metnini al
+            string selectedText = listView1.SelectedItems[0].SubItems[2].Text;
+
+            // Mesaj kutusunda seçilen öğenin metnini göster
+            MessageBox.Show(selectedText);
+        }
+        #endregion
+
+        #region btnYeniNotEkle_Click
+        private void btnYeniNotEkle_Click(object sender, EventArgs e)
+        {
+            YeniNotEkle();
+        }
+        #endregion
+
+        #region NotuSilBtn_Click
+        private void NotuSilBtn_Click(object sender, EventArgs e)
+        {
+            NotuSil();
+        }        
+        #endregion
+
+        #region listView1_SelectedIndexChanged
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                NotuSilBtn.Visible = NotuGoruntuleBtn.Visible = notuDuzenleBtn.Visible = true;
+            }
+            else
+            {
+                NotuSilBtn.Visible = NotuGoruntuleBtn.Visible = notuDuzenleBtn.Visible = false;
+            }
+        }
+        #endregion
+
+        private void notuDuzenleBtn_Click(object sender, EventArgs e)
+        {            
+            if (listView1.SelectedItems.Count == 1)
+            {
+                
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                string detaylar = selectedItem.SubItems[2].Text;
+                string baslik = selectedItem.SubItems[1].Text;
+
+                // Yeni bir form aç
+                Form notuDuzenleForm = new Form();
+                notuDuzenleForm.Text = "Notu Düzenle";
+
+                // Not başlığı için metin kutusu oluştur
+                Label lblBaslik = new Label();
+                lblBaslik.Text = "Başlık:";
+                lblBaslik.Location = new Point(10, 10);
+                notuDuzenleForm.Controls.Add(lblBaslik);
+
+                TextBox txtBaslik = new TextBox();
+                txtBaslik.Text = baslik;
+                txtBaslik.Location = new Point(10, 30);
+                notuDuzenleForm.Controls.Add(txtBaslik);
+
+
+                // Not detayları için metin alanı oluştur
+                Label lblDetaylar = new Label();
+                lblDetaylar.Text = "Detaylar:";
+                lblDetaylar.Location = new Point(10, 60);
+                notuDuzenleForm.Controls.Add(lblDetaylar);
+
+                TextBox txtDetaylar = new TextBox();
+                txtDetaylar.Multiline = true;
+                txtDetaylar.Text = detaylar;
+                txtDetaylar.Size = new Size(200, 100);
+                txtDetaylar.Location = new Point(10, 80);
+                notuDuzenleForm.Controls.Add(txtDetaylar);
+
+
+
+                // Kaydet butonu oluştur
+                Button btnKaydet = new Button();
+                btnKaydet.Text = "Kaydet";
+                btnKaydet.Location = new Point(10, 190);
+                notuDuzenleForm.Controls.Add(btnKaydet);
+
+                // Kaydet butonuyla notu listview'e ekle
+                btnKaydet.Click += (sender2, e2) =>
+                {
+                    NotuSil();
+                    DateTime txtTarih = dtp.Value;
+                    ListViewItem item = new ListViewItem();
+                    item.Text = txtBaslik.Text + " | " + dtp.Value.ToShortDateString();
+                    item.SubItems.Add(txtBaslik.Text);
+                    item.SubItems.Add(txtDetaylar.Text);
+                    item.SubItems.Add(txtTarih.ToShortDateString());
+                    listView1.Items.Add(item);
+
+                    // Notları dosyaya kaydet
+                    string dosyaYolu = "notlar.txt";
+                    using (StreamWriter sw = new StreamWriter(dosyaYolu, true))
+                    {
+                        sw.WriteLine("$" + txtTarih.ToShortDateString() + ";" + txtBaslik.Text + ";" + txtDetaylar.Text);
+                    }
+
+                    notuDuzenleForm.Close(); // Formu kapat
+                };
+
+
+
+
+                //if (form.ShowDialog() == DialogResult.OK)
+                //{
+                //    selectedItem.SubItems[2].Text = form.textBox1.Text;
+                //    // Kayıt dosyasını güncelle
+                //    string[] lines = File.ReadAllLines("notlar.txt");
+                //    for (int i = 0; i < lines.Length; i++)
+                //    {
+                //        if (lines[i].StartsWith("$" + selectedItem.SubItems[0].Text))
+                //        {
+                //            lines[i] = "$" + selectedItem.SubItems[0].Text + ";" + selectedItem.SubItems[1].Text + ";" + selectedItem.SubItems[2].Text + ";" + selectedItem.SubItems[3].Text + ";";
+                //            break;
+                //        }
+                //    }
+                //    File.WriteAllLines("notlar.txt", lines);
+                //}
+
+                notuDuzenleForm.ShowDialog(); // Formu göster
+            }
+        }
+
+        private void NotuGoruntuleBtn_Click(object sender, EventArgs e)
+        {
+            // Seçilen öğenin metnini al
+            string selectedText = listView1.SelectedItems[0].SubItems[2].Text;
+
+            // Mesaj kutusunda seçilen öğenin metnini göster
+            MessageBox.Show(selectedText);
+        }
+    }
+}
